c1c1351e6e81325aca96f762f1e88fd3c57f4f2e
Sheng Liu <shengliu@Shengs-MacBook-Pro.local>
2018-02-20 18:36:20 -0500

    Multiple browse supported

--- src/App.js
+++ src/App.js
@@ -104,6 +104,11 @@ class App extends Component {
         <div className="App-main">
             <div className="container-fluid" >
                 <nav className="navbar nav-bar navbar-inverse navbar-fixed-top">
+                    <div className="navbar-header">
+
+                    </div>
+
+                    <div className="collapse navbar-collapse">
                     <ul className="nav navbar-nav" >
                         <li className="nav-item" >
                             <a className="nav-link" href="#">Main </a></li>
@@ -116,12 +121,10 @@ class App extends Component {
                                                     <li className="nav-item">
                             <a className="nav-link" href="#Goal">Goal</a></li>*/}
                     </ul>
-
+                    </div>
                 </nav>
 
-                
-                <div className="container-fluid-self about-bg">
-
+                <div className="container-fluid-self">
                     <img className = "profileIMG-circle" src={this.state.sheng_img} alt="Sheng Liu" />
                     <br/>
                     <h2>> Hello, I am Sheng Liu</h2>
@@ -210,10 +213,10 @@ class App extends Component {
                         <div className="div-right">
                             <i><h4 className="div-title">Data Analyst, Peer Mentor</h4></i>
                             <h5>Borough of Manhattan Community College,New York, NY, Feburary 2016 - Present</h5>
-                            <p>
-                                Work in a group size of over 8 Data Analysts. Constantly coorperate with leadership<br/>
-                                team to analyst over 80 mentors log data, and generate useful report by using statistical<br/>
-                                method. Help mentees to increase self-confidence and self-esteem by envolving <br/>
+                            <p className="job-description">
+                                Work in a group size of over 8 Data Analysts. Constantly coorperate with leadership
+                                team to analyst over 80 mentors log data, and generate useful report by using statistical
+                                method. Help mentees to increase self-confidence and self-esteem by envolving
                                 campus programs and activities. 
 
                             </p>
@@ -222,10 +225,10 @@ class App extends Component {
                             
                             <i><h4 className="div-title">Computer Science Tutor</h4></i>
                             <h5>CUNY Research Foundation,New York, NY, Feburary 2017 - Present</h5>
-                            <p>
-                                Instruct middle school students on basic math and algorithm behind coding. One-to-group<br/>
-                                tutoring methology in HTML, CSS, Scratch, and Codester.com challenges. Cooperate host<br/>
-                                teacher to analyst and discuss each student's academic performance, and suggestions to<br/>
+                            <p className="job-description">
+                                Instruct middle school students on basic math and algorithm behind coding.
+                                One-to-group tutoring methology in HTML, CSS, Scratch, and Codester.com challenges. Cooperate host
+                                teacher to analyst and discuss each student's academic performance, and suggestions to
                                 create lesson plan.
 
                             </p>
@@ -234,7 +237,7 @@ class App extends Component {
 
                             <i><h4 className="div-title">Registrar Assistant</h4></i>
                             <h5>Borough of Manhattan Community College,New York, NY, May 2017 - Auguest 2017</h5>
-                            <p>
+                            <p className="job-description">
                                 Publicly presented with over 30 freshman students on the use of CUNY online services.
                                 Worked one-to-one with student to troubleshoot registration hold and error. 
 
@@ -252,7 +255,7 @@ class App extends Component {
             <div className="container-fluid-self contact-bg" id="contact">
                
                 <div className="content">
-                    <h2 className="margin">Aspiring to become a Software Engineer.</h2>
+                    <h2>Aspiring to become a Software Engineer.</h2>
                     <ul className="social-icon-list">
                         <li className="social-icon-item"> 
                             <a className="link" href="http://github.com/sheng1302" >
@@ -265,7 +268,7 @@ class App extends Component {
                         </li>
 
                     </ul>
-                    <p>© 2017 All rights reserved.</p>
+                    <p>© 2018 All rights reserved.</p>
                 </div>
 
                 
